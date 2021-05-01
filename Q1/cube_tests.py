62527a9d0cbde1939fe2e0f1e00203c12de24ba1
Tanner <fossta@oregonstate.edu>
2021-05-01 11:53:22 -0700

    added test explanations

--- Q1/cube_tests.py
+++ Q1/cube_tests.py
@@ -3,16 +3,18 @@ from cube import *
 
 
 class TestCase(unittest.TestCase):
+
+    # test if volume is calculated properly
     def test_pos(self):
         self.assertAlmostEqual(volume(2), 8)
-        self.assertAlmostEqual(volume(4), 64)
 
+    # make sure negative input is handled
     def test_neg(self):
         self.assertAlmostEqual(volume(-2), 8)
 
+    # make sure non int or float input is handled
     def test_input_not_int(self):
         self.assertAlmostEqual(volume("asdf"), 8)
-        self.assertRaises(TypeError, volume, True)
 
 
 if __name__ == "__main__":
