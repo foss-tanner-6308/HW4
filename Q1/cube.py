63b56472c403875f868a2a40a06d6f85620b41ef
Tanner <fossta@oregonstate.edu>
2021-05-01 11:29:55 -0700

    cube and test files

--- Q1/cube.py
+++ Q1/cube.py
@@ -1,1 +1,7 @@
-
+def volume(a):
+    if type(a) not in [int, float]:
+        raise TypeError("Intergers only.")
+    if a > 0:
+        return a ** 3
+    if a < 0:
+        raise ValueError("Positive integers only.")
--- Q1/cube_tests.py
+++ Q1/cube_tests.py
@@ -1,1 +1,19 @@
+import unittest
+from cube import *
 
+
+class TestCase(unittest.TestCase):
+    def test_pos(self):
+        self.assertAlmostEqual(volume(2), 8)
+        self.assertAlmostEqual(volume(4), 64)
+
+    def test_neg(self):
+        self.assertAlmostEqual(volume(-2), 8)
+
+    def test_input_not_int(self):
+        self.assertAlmostEqual(volume("asdf"), 8)
+        self.assertRaises(TypeError, volume, True)
+
+
+if __name__ == "__main__":
+    unittest.main()
