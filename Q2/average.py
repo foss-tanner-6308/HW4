57fb3845f9e68d4954684b8d0d50b0d89a5b0c16
Tanner <fossta@oregonstate.edu>
2021-05-01 11:49:11 -0700

    average and test files

--- Q2/average.py
+++ Q2/average.py
@@ -1,1 +1,5 @@
-
+def avg(lst):
+    if (len(lst)) == 0:
+        raise ValueError("Cannot divide by 0 (zero).")
+    avg = sum(lst) / len(lst)
+    return avg
--- Q2/average_tests.py
+++ Q2/average_tests.py
@@ -1,1 +1,17 @@
+import unittest
+from average import *
 
+
+class TestCase(unittest.TestCase):
+    def test_list_good(self):
+        self.assertAlmostEqual(avg([1, 1, 1, 3, 4]), 2)
+
+    def test_list_bad(self):
+        self.assertAlmostEqual(avg([1, 1, 1, 1, 1]), 5)
+
+    def test_empty_zero(self):
+        self.assertAlmostEqual(avg([]), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
