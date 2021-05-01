a7e8ab198ec868edbb6d6d31e921ed5413e3ffe7
Tanner <fossta@oregonstate.edu>
2021-05-01 12:16:36 -0700

    name and test files

--- Q3/name.py
+++ Q3/name.py
@@ -1,1 +1,16 @@
+# helper function taken from stack overflow because I could not get, for example,
+# first.isalpha(), first.isdigit(), and first.isalnum() to work properly. These
+# alone would cause all tests to fail so I looked for an alternative, which used
+# isdigit() in a loop
 
+# https://stackoverflow.com/questions/19859282/check-if-a-string-contains-a-number
+def hasNumbers(s):
+    return any(i.isdigit() for i in s)
+
+
+def full_name(first, last):
+    if hasNumbers(first):
+        raise ValueError("Name can only be made of letters.")
+    if hasNumbers(last):
+        raise ValueError("Name can only be made of letters.")
+    return first + " " + last
--- Q3/name_tests.py
+++ Q3/name_tests.py
@@ -1,1 +1,21 @@
+import unittest
+from name import *
 
+
+class TestCase(unittest.TestCase):
+
+    # test if name created properly
+    def test_works(self):
+        self.assertEqual(full_name("Tanner", "Foss"), "Tanner Foss")
+
+    # test if incorrect expected output is caught
+    def test_bad(self):
+        self.assertEqual(full_name("Tanner", "Foss"), "Tanner Jacob")
+
+    # test if numbers in the name are caught
+    def test_int(self):
+        self.assertEqual(full_name("Tann3r", "F0ss"), "Tann3r F0ss")
+
+
+if __name__ == "__main__":
+    unittest.main()
