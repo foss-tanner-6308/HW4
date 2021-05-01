6d2e223a801f5325c8a1a72cfcc7e6bfb57ecf4a
Tanner <fossta@oregonstate.edu>
2021-05-01 11:51:29 -0700

    added test explanations

--- Q2/average_tests.py
+++ Q2/average_tests.py
@@ -3,12 +3,16 @@ from average import *
 
 
 class TestCase(unittest.TestCase):
+
+    # test if average calculated properly
     def test_list_good(self):
         self.assertAlmostEqual(avg([1, 1, 1, 3, 4]), 2)
 
+    # test if incorrect expected output recognized
     def test_list_bad(self):
         self.assertAlmostEqual(avg([1, 1, 1, 1, 1]), 5)
 
+    # make sure empty list/divide by 0 is handled
     def test_empty_zero(self):
         self.assertAlmostEqual(avg([]), 0)
 
